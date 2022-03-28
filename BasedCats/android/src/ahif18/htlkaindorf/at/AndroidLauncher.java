4444e4643c9b7e7c2df13102469a06612beafbf1
Pavica <81927158+Pavica@users.noreply.github.com>
2022-03-28 11:27:51 +0200

    Add files via upload

---
+++ BasedCats/android/src/ahif18/htlkaindorf/at/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ahif18.htlkaindorf.at;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ahif18.htlkaindorf.at.BasedCats;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BasedCats(), config);
+	}
+}
