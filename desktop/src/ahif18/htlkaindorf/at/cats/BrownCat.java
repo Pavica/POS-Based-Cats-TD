64634b73612d17f5421d5bedea0634b131cf0e66
MoonBunny <81986945+Clarki28@users.noreply.github.com>
2022-05-02 11:43:52 +0200

    Dynamic Cat generating

---
+++ desktop/src/ahif18/htlkaindorf/at/cats/BrownCat.java
@@ -0,0 +1,10 @@
+package ahif18.htlkaindorf.at.cats;
+
+import com.badlogic.gdx.math.Rectangle;
+
+public class BrownCat extends Cat{
+
+    public BrownCat(Rectangle range, Rectangle body) {
+        super(range, body, 10, 50);
+    }
+}
