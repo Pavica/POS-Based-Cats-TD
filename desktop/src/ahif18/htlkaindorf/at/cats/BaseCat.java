f872a5440e3ad91af259cd1b9b2f06d5af241e55
Pavica <81927158+Pavica@users.noreply.github.com>
2022-05-09 11:34:42 +0200

    added range difference, click on cats to see body and range, changed cat structure

--- desktop/src/ahif18/htlkaindorf/at/cats/BaseCat.java
+++ desktop/src/ahif18/htlkaindorf/at/cats/BaseCat.java
@@ -5,7 +5,15 @@ import com.badlogic.gdx.math.Rectangle;
 public class BaseCat extends Cat{
 
     public static int COST = 100;
-    public BaseCat(Rectangle range, Rectangle body) {
-        super(range, body, 100, 1000, 0, 1);
+    public static int RANGE_WIDTH = 150;
+    public static int RANGE_HEIGHT = 150;
+    public BaseCat(float x, float y) {
+        super(
+            new Rectangle(x - RANGE_WIDTH/2,y - RANGE_HEIGHT/2,RANGE_WIDTH,RANGE_HEIGHT),
+            new Rectangle(x- CAT_BODY_WIDTH/2,y - CAT_BODY_HEIGHT/2,CAT_BODY_WIDTH,CAT_BODY_HEIGHT),
+            1000,
+            500,
+            0,
+            0);
     }
 }
--- desktop/src/ahif18/htlkaindorf/at/cats/MooCat.java
+++ desktop/src/ahif18/htlkaindorf/at/cats/MooCat.java
@@ -5,7 +5,14 @@ import com.badlogic.gdx.math.Rectangle;
 public class MooCat extends Cat{
 
     public static int COST = 200;
-    public MooCat(Rectangle range, Rectangle body) {
-        super(range, body, 200, 500, 3,1);
+    public static int RANGE_WIDTH = 200;
+    public static int RANGE_HEIGHT = 200;
+    public MooCat(float x, float y) {
+        super(new Rectangle(x - RANGE_WIDTH/2,y - RANGE_HEIGHT/2,RANGE_WIDTH,RANGE_HEIGHT),
+                new Rectangle(x- CAT_BODY_WIDTH/2,y - CAT_BODY_HEIGHT/2,CAT_BODY_WIDTH,CAT_BODY_HEIGHT),
+                200,
+                500,
+                3,
+                1);
     }
 }
